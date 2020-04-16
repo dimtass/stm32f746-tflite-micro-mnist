22d46609a6ecd0b51fd2f3b109bd3bf271058c33
Dimitris Tassopoulos <dimtass@gmail.com>
2020-04-16 23:08:50 +0200

    Added a docker-build script

---
+++ docker-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+#
+# Example:
+#   ./docker-build.sh "CLEANBUILD=true USE_HAL_DRIVER=ON USE_OVERCLOCK=OFF USE_CMSIS_NN=OFF USE_COMP_MODEL=ON SRC=src ./build.sh"
+
+docker run --rm -it -v $(pwd):/tmp/tflite -w=/tmp/tflite dimtass/stm32-cde-image:0.1 -c "${1}"
